6255204de1f03673f0fa192521ada482fe659b89
Michal Molenda <michalus88@gmail.com>
2022-03-08 21:56:21 +0100

    fead: added create user method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ResponseUserData } from 'src/interfaces/user';
 
 import { User } from 'src/schemas/user.schema';
+import { hashPwd } from 'src/utils/hash-pwd';
+import { sanitizeUser } from 'src/utils/sanitize-user';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async create(email: string, pwd: string): Promise<ResponseUserData> {
+    const hashedPwd = hashPwd(pwd);
+    const newUser = new this.userModel({ email, pwd: hashedPwd }) as User;
+    await newUser.save();
+    return sanitizeUser(newUser);
+  }
 }
