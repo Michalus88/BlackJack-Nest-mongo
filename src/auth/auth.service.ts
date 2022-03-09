a05c1ce1fc2cdb331d8e3ed8a298ae71091e251b
Michal Molenda <michalus88@gmail.com>
2022-03-09 02:14:48 +0100

    fead: created validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { hashPwd } from 'src/utils/hash-pwd';
+import { sanitizeUser } from 'src/utils/sanitize-user';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UserService) {}
+
+  async validateUser(email: string, pwd: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && user.pwd === hashPwd(pwd)) {
+      return sanitizeUser(user);
+    }
+    return null;
+  }
+}
