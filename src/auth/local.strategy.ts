a2638e162987937ed50a0ab93b60705a3d8c2ec4
Michal Molenda <michalus88@gmail.com>
2022-03-09 02:15:57 +0100

    fead: created validate method

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -10,5 +10,13 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
       usernameField: 'email',
       passwordField: 'pwd',
     });
+  }
+
+  async validate(email: string, pwd: string): Promise<any> {
+    const user = await this.authService.validateUser(email, pwd);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
