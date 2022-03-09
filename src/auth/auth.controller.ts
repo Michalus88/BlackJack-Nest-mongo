1397b909ef4fa59974d33295f64957899fc4324b
Michal Molenda <michalus88@gmail.com>
2022-03-09 15:05:33 +0100

    fead: implemented login method and localAuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+
+import { AuthService } from './auth.service';
+import { UserData } from 'src/interfaces/user';
+import { LocalAuthGuard } from './local-auth.guard';
+import { UserObj } from 'src/decorators/user-object.decorator';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@UserObj() user: UserData, @Res() res: Response) {
+    return await this.authService.login(user, res);
+  }
+}
