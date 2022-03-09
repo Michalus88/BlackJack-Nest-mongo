1c8065abdc1be87a0dcfb55444d084090aade3e6
Michal Molenda <michalus88@gmail.com>
2022-03-09 23:14:02 +0100

    fead: created and implemented shuffle method

--- src/deck/deck.service.ts
+++ src/deck/deck.service.ts
@@ -10,6 +10,17 @@ export class DeckService {
     cardsTypes.forEach((type) =>
       cardsWeights.forEach((weight) => deck.push({ weight, type })),
     );
-    return deck;
+    return this.shuffle(deck);
+  }
+
+  shuffle(deck: CardInterface[]) {
+    const cards = deck;
+    for (let i = deck.length - 1; i >= 0; i--) {
+      const randomCardIndex = Math.floor(Math.random() * i);
+      const currentIterationCard = deck[i];
+      deck[i] = deck[randomCardIndex];
+      deck[randomCardIndex] = currentIterationCard;
+    }
+    return cards;
   }
 }
