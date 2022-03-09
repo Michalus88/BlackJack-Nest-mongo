d0b14054d78f683f863c1bea6556c582b5553ea3
Michal Molenda <michalus88@gmail.com>
2022-03-09 02:05:16 +0100

    fead: added UserModule to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [PassportModule],
+  imports: [UserModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
