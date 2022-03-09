49745c961c80d0e02760eb639fea794c5833d0d1
Michal Molenda <michalus88@gmail.com>
2022-03-09 22:57:23 +0100

    fead: created metod createdDeck

--- src/deck/deck.service.ts
+++ src/deck/deck.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CardInterface } from 'src/schemas/user.schema';
+import { cardsTypes, cardsWeights } from './constans';
 
 @Injectable()
-export class DeckService {}
+export class DeckService {
+  createDeck() {
+    const deck: CardInterface[] = [];
+
+    cardsTypes.forEach((type) =>
+      cardsWeights.forEach((weight) => deck.push({ weight, type })),
+    );
+    return deck;
+  }
+}
