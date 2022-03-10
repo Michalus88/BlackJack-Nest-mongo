4c7446cb2f6b848d9c51f303d41c70f38af9fafc
Michal Molenda <michalus88@gmail.com>
2022-03-10 17:19:17 +0100

    fead: implemented initialize method and JwtAuthGuard

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserObj } from 'src/decorators/user-object.decorator';
+import { UserData } from 'src/interfaces/user';
+import { GameService } from './game.service';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+  constructor(private gameService: GameService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async initialize(@UserObj() user: UserData) {
+    return this.gameService.initialize(user);
+  }
+}
