2722d8cb35cf58e3f4911dcfb49eced6f14ae45f
Michal Molenda <michalus88@gmail.com>
2022-03-10 17:10:32 +0100

    fead: created methods initialize and pickCard

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { DeckService } from 'src/deck/deck.service';
+import { UserData } from 'src/interfaces/user';
+import { CardInterface } from 'src/schemas/user.schema';
+import { UserService } from 'src/user/user.service';
+import { sanitizeUser } from 'src/utils/sanitize-user';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+  constructor(
+    private userService: UserService,
+    private deckService: DeckService,
+  ) {}
+  async initialize(user: UserData) {
+    const player = await this.userService.findByEmail(user.email);
+    const { deck, playerCards, dealerCards } = player;
+
+    if (deck.length === 0) {
+      deck.push(...this.deckService.createDeck());
+    }
+    if (!player.isDeal) {
+      for (let i = 0; i < 2; i++) {
+        playerCards.push(this.pickCard(player.deck));
+      }
+      dealerCards.push(this.pickCard(player.deck));
+    }
+    player.isDeal = true;
+    player.save();
+    console.log(player);
+
+    return sanitizeUser(player);
+  }
+
+  pickCard(deck: CardInterface[]) {
+    if (deck.length < 10) {
+      deck = this.deckService.createDeck();
+    }
+    const card = deck.pop();
+    return card;
+  }
+}
