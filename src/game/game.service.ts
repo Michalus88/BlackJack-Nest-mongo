b9971644ac6f1b94674c8b0e0b8dfdabf8798562
Michal Molenda <michalus88@gmail.com>
2022-03-13 23:36:45 +0100

    Refactor splited the code from GameService to services Player and Deck

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DeckService } from 'src/deck/deck.service';
 import { UserData } from 'src/interfaces/user';
+import { PlayerService } from 'src/player/player.service';
 import { CardInterface } from 'src/schemas/user.schema';
 import { UserService } from 'src/user/user.service';
 import { sanitizeUser } from 'src/utils/sanitize-user';
@@ -10,58 +11,14 @@ export class GameService {
   constructor(
     private userService: UserService,
     private deckService: DeckService,
+    private playerService: PlayerService,
   ) {}
   async initialize(user: UserData) {
     const player = await this.userService.findByEmail(user.email);
-    this.dealCards(player);
-    this.calculatePoints(player);
+    this.deckService.dealCards(player);
+    this.playerService.calculatePoints(player);
 
-    player.save();
+    await player.save();
     return sanitizeUser(player);
-  }
-
-  dealCards(player: UserData) {
-    const { deck, playerCards, dealerCards, isDeal } = player;
-
-    if (deck.length === 0) {
-      deck.push(...this.deckService.createDeck());
-    }
-    if (!isDeal) {
-      for (let i = 0; i < 2; i++) {
-        playerCards.push(this.pickCard(deck));
-      }
-      dealerCards.push(this.pickCard(deck));
-    }
-    player.isDeal = true;
-  }
-
-  pickCard(deck: CardInterface[]) {
-    if (deck.length < 10) {
-      deck.push(...this.deckService.createDeck());
-    }
-    const card = deck.pop();
-    return card;
-  }
-
-  calculatePoints(player: UserData): void {
-    player.playerPoints = this.addPoints(player.playerCards);
-    player.dealerPoints = this.addPoints(player.dealerCards);
-  }
-
-  addPoints(cards): number {
-    let points = 0;
-    cards.forEach((card) => {
-      const weight =
-        card.weight === 'J' ||
-        card.weight === 'D' ||
-        card.weight === 'K' ||
-        card.weight === 'A'
-          ? card.weight === 'A' && cards.length > 2
-            ? 1
-            : 10
-          : card.weight;
-      points += Number(weight);
-    });
-    return points;
   }
 }
