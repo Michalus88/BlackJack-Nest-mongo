6bd8ef0e39469efbde32d01d47d99cc74048cf98
Michal Molenda <michalus88@gmail.com>
2022-03-13 23:39:21 +0100

    fead: imported PlayerService

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,11 +1,16 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { DeckModule } from 'src/deck/deck.module';
+import { PlayerModule } from 'src/player/player.module';
 import { UserModule } from 'src/user/user.module';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
 
 @Module({
-  imports: [forwardRef(() => DeckModule), forwardRef(() => UserModule)],
+  imports: [
+    forwardRef(() => DeckModule),
+    forwardRef(() => UserModule),
+    forwardRef(() => PlayerModule),
+  ],
   controllers: [GameController],
   providers: [GameService],
 })
