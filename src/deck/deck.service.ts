620981278fe87eb71bfe819b5c864f904fa3819d
Michal Molenda <michalus88@gmail.com>
2022-03-13 23:36:45 +0100

    Refactor splited the code from GameService to services Player and Deck

--- src/deck/deck.service.ts
+++ src/deck/deck.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { UserData } from 'src/interfaces/user';
 import { CardInterface } from 'src/schemas/user.schema';
 import { cardsTypes, cardsWeights } from './constans';
 
 @Injectable()
 export class DeckService {
-  createDeck() {
-    const deck: CardInterface[] = [];
+  dealCards(player: UserData) {
+    const { deck, playerCards, dealerCards, isDeal } = player;
 
+    if (deck.length === 0) {
+      deck.push(...this.createDeck());
+    }
+    if (!isDeal) {
+      for (let i = 0; i < 2; i++) {
+        playerCards.push(this.pickCard(deck));
+      }
+      dealerCards.push(this.pickCard(deck));
+    }
+    player.isDeal = true;
+  }
+
+  createDeck(): CardInterface[] {
+    const deck: CardInterface[] = [];
     cardsTypes.forEach((type) =>
       cardsWeights.forEach((weight) => deck.push({ weight, type })),
     );
+
     return this.shuffle(deck);
   }
 
-  shuffle(deck: CardInterface[]) {
+  shuffle(deck: CardInterface[]): CardInterface[] {
     const cards = deck;
     for (let i = deck.length - 1; i >= 0; i--) {
       const randomCardIndex = Math.floor(Math.random() * i);
@@ -21,6 +37,16 @@ export class DeckService {
       deck[i] = deck[randomCardIndex];
       deck[randomCardIndex] = currentIterationCard;
     }
+
     return cards;
+  }
+
+  pickCard(deck: CardInterface[]): CardInterface {
+    if (deck.length < 10) {
+      deck.push(...this.createDeck());
+    }
+    const card = deck.pop();
+
+    return card;
   }
 }
--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { UserData } from 'src/interfaces/user';
+import { CardInterface } from 'src/schemas/user.schema';
 
 @Injectable()
-export class PlayerService {}
+export class PlayerService {
+  calculatePoints(player: UserData): void {
+    player.playerPoints = this.addPoints(player.playerCards);
+    player.dealerPoints = this.addPoints(player.dealerCards);
+  }
+
+  addPoints(cards): number {
+    let points = 0;
+    cards.forEach((card) => {
+      const weight =
+        card.weight === 'J' ||
+        card.weight === 'D' ||
+        card.weight === 'K' ||
+        card.weight === 'A'
+          ? card.weight === 'A' && cards.length > 2
+            ? 1
+            : 10
+          : card.weight;
+      points += Number(weight);
+    });
+    return points;
+  }
+}
