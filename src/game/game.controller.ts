0484aa48656bfe67e9e6e763309b56b90ac6a267
Michal Molenda <michalus88@gmail.com>
2022-03-14 10:06:03 +0100

    fead: implemented bet dto, changed Post on Put

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UserObj } from 'src/decorators/user-object.decorator';
 import { UserData } from 'src/interfaces/user';
+import { PlayerBetDto } from './dto/player-bet.dto';
 import { GameService } from './game.service';
 
 @Controller('game')
@@ -12,5 +13,11 @@ export class GameController {
   @Get()
   async initialize(@UserObj() user: UserData) {
     return this.gameService.initialize(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('bet')
+  async setBet(@UserObj() user: UserData, @Body() playerBetDto: PlayerBetDto) {
+    return this.gameService.setBet(user, playerBetDto);
   }
 }
