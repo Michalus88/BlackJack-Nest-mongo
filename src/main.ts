338d5d4f6ec26b30deb59df987b3b87ab81bb34e
Michal Molenda <michalus88@gmail.com>
2022-03-21 21:43:36 +0100

    fead: process.env.PORT set

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
