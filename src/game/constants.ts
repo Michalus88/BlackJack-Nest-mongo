2558895141fb78094e78dc8ad80964c627bb03fc
Michal Molenda <michalus88@gmail.com>
2022-07-14 21:09:26 +0200

    added enums and fixed counting points

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -8,3 +8,10 @@ export enum GameResults {
 export const MAX_NUMBER_OF_POINTS = 21;
 export const NUMBER_TO_WHICH_DEALER_MUST_TAKE_CARD = 17;
 export const ALLOCATED_FUNDS = 500;
+
+export enum CardSymbols {
+  KING = 'K',
+  QUEEN = 'Q',
+  JACK = 'J',
+  ACE = 'A',
+}
