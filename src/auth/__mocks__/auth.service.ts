65c7f26dd2e42ca19e236a80c29a89813ba40415
Michal Molenda <michalus88@gmail.com>
2022-10-05 12:03:03 +0200

    added mock implementation for validateUser

--- src/auth/__mocks__/auth.service.ts
+++ src/auth/__mocks__/auth.service.ts
@@ -1,7 +1,15 @@
 import { loggedUserResStub, userStub } from '../../user/test/stubs/user.sub';
 
 export const AuthService = jest.fn().mockReturnValue({
-  validateUser: jest.fn().mockResolvedValue(userStub() || null),
+  validateUser: jest
+    .fn()
+    .mockImplementation(async (email: string, pwd: string) => {
+      if (email === userStub().email && pwd === userStub().pwd) {
+        return userStub();
+      } else {
+        return null;
+      }
+    }),
   login: jest.fn().mockResolvedValue({ json: loggedUserResStub() }),
   logout: jest
     .fn()
