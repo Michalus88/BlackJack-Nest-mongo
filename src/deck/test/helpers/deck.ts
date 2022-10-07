bb700d595d65f229f97907b9155c4d17d556b523
Michal Molenda <michalus88@gmail.com>
2022-10-07 12:45:18 +0200

    implemented unit tests

---
+++ src/deck/test/helpers/deck.ts
@@ -0,0 +1,10 @@
+import { CARDS_WEIGHTS, CARD_TYPES } from '../../constans';
+import { CardInterface } from '../../../schemas/user.schema';
+
+export const noShuffleDeck = () => {
+  const deck: CardInterface[] = [];
+  CARD_TYPES.forEach((type) =>
+    CARDS_WEIGHTS.forEach((weight) => deck.push({ weight, type })),
+  );
+  return deck;
+};
